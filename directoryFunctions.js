115355d16a9b237b6bf92578df2a6ecdf630ea84
Colten-Long <colten.long@gmail.com>
2016-11-21 17:16:39 -0700

    Add files via upload

--- directoryFunctions.js
+++ directoryFunctions.js
@@ -1,9 +1,20 @@
+// Global Variables
 var character = [];
 
+// Debug Functions
+function alert(message) { document.getElementById("alertBox").innerHTML = message; }
+
+// Data Functions
 function loadCharacters()
 {
-  {{foreach $characters as $character}}
-    character.push(\{"fullName":"{$character_full_name}","firstName":"{$character_first_name}","lastName":"{$character_last_name}","animal":"{$character_animal}","ethics":"{$character_ethics}","morals":"{$character_morals}"\});
+  var output = "";
+  {{foreach $record as $character}}
+    output += {$character_full_name};
+    output += {$character=>character_full_name};
+    output += {$character->customFieldDisplayByKey('character_full_name', 'listing')};
+    output += {$character->customFieldDisplayByKey('character_full_name', 'display')};
+    //{$character->customFieldDisplayByKey('character_full_name', 'listing')|raw} //character.push(\{"fullName":"{$character_full_name}","firstName":"{$character_first_name}","lastName":"{$character_last_name}","animal":"{$character_animal}","ethics":"{$character_ethics}","morals":"{$character_morals}"\});
   {{endforeach}}
-  document.write(character.join(", "));
+  //document.write(character.join(", "));
+  alert(output);
 }
