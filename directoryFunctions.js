634a8ba7c2d8ca1339911ef3b4cd6fd56160e392
Colten-Long <colten.long@gmail.com>
2016-11-21 21:40:19 -0700

    Add files via upload

--- directoryFunctions.js
+++ directoryFunctions.js
@@ -10,7 +10,9 @@ function loadCharacters()
   var output = "";
   {{foreach $record as $character}}
     output += {$character_full_name};
-    output += {$character=>character_full_name};
+    output += {$character=>character_full_name};//Note the equal sign in the pipe.
+    output += {$character->character_full_name};//Note the minus sign in the pipe.
+    output += {$character=>customFieldDisplayByKey('character_full_name', 'listing')};
     output += {$character->customFieldDisplayByKey('character_full_name', 'listing')};
     output += {$character->customFieldDisplayByKey('character_full_name', 'display')};
     //{$character->customFieldDisplayByKey('character_full_name', 'listing')|raw} //character.push(\{"fullName":"{$character_full_name}","firstName":"{$character_first_name}","lastName":"{$character_last_name}","animal":"{$character_animal}","ethics":"{$character_ethics}","morals":"{$character_morals}"\});
